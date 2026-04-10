112681d09cd7114630dee05223ab8eaf4285f9ca
Alan Night <a.nightails@gmail.com>
2026-04-10 17:26:08 -0500

    Handle multi-line reading and buffer split logic for file parsing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,11 +16,21 @@ func main() {
 	defer file.Close()
 
 	buff := make([]byte, 8)
+	var currentLine string
 
 	for {
 		n, err := io.ReadFull(file, buff)
 		if n > 0 {
-			fmt.Printf("read: %s\n", string(buff[:n]))
+			b := string(buff[:n])
+			if strings.Contains(b, "\n") {
+				parts := strings.Split(b, "\n")
+				for i := 0; i < len(parts)-1; i++ {
+					fmt.Printf("read: %s\n", currentLine+parts[i])
+				}
+				currentLine = parts[len(parts)-1]
+			} else {
+				currentLine += b
+			}
 		}
 
 		if err == io.EOF || errors.Is(err, io.ErrUnexpectedEOF) {
@@ -28,5 +39,9 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+	}
+
+	if currentLine != "" {
+		fmt.Printf("read: %s\n", currentLine)
 	}
 }
