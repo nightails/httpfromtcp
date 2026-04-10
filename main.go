d3b8fbc5f429c8199d8eb2ffd95f8a0f2ed3ef3d
Alan Night <a.nightails@gmail.com>
2026-04-10 16:58:34 -0500

    Add file reading logic to process messages from "messages.txt"

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Print("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	buff := make([]byte, 8)
+
+	for {
+		n, err := io.ReadFull(file, buff)
+		if n > 0 {
+			fmt.Printf("read: %s\n", string(buff[:n]))
+		}
+
+		if err == io.EOF || errors.Is(err, io.ErrUnexpectedEOF) {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
 }
