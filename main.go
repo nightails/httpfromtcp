2ef5a54ac8d55c96ee2cd9c082c91962ea5ee6f2
Alan Night <a.nightails@gmail.com>
2026-04-11 10:10:33 -0500

    Refactor file reading logic by introducing `getLineChannel` for cleaner processing and improved readability

--- main.go
+++ main.go
@@ -9,34 +9,50 @@ import (
 	"strings"
 )
 
+const fileInput = "messages.txt"
+
 func main() {
-	file, err := os.Open("messages.txt")
+	file, err := os.Open(fileInput)
 	if err != nil {
-		log.Fatalf("error opening messages.txt: %s\n", err)
+		log.Fatalf("error opening %s: %s\n", fileInput, err.Error())
 	}
 	defer file.Close()
 
-	var currentLine string
-	for {
-		buffer := make([]byte, 8, 8)
-		n, err := file.Read(buffer)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
+	ch := getLineChannel(file)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLineChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLine != "" {
+					ch <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
 		}
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", currentLine+parts[i])
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
-	}
+	}()
+	return ch
 }
