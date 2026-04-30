faf27c1281301ce6119e7dc802a67c66194c9922
Alan Night <a.nightails@gmail.com>
2026-04-30 09:32:37 -0500

    Add `Headers` struct with parsing logic and corresponding tests

---
+++ internal/headers/headers.go
@@ -0,0 +1,11 @@
+package headers
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	return 0, false, err
+}
