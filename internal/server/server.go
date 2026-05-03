9f28fb4b815a14d62b6ca2735502984bc066eb64
Alan Night <a.nightails@gmail.com>
2026-05-03 16:32:34 -0500

    Implement basic TCP server with `Serve`, `Close`, and request handling

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,63 @@
 package server
 
-import "net"
+import (
+	"log"
+	"net"
+	"strconv"
+	"sync/atomic"
+)
 
 type Server struct {
+	Listener net.Listener
+	closed   atomic.Bool
 }
 
 func Serve(port int) (*Server, error) {
-	return &Server{}, nil
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
+	srv := &Server{Listener: ln}
+	go srv.listen()
+	return srv, nil
 }
 
 func (s *Server) Close() error {
+	if s.closed.Swap(true) {
+		return nil
+	}
+
+	if s.Listener != nil {
+		return s.Listener.Close()
+	}
 	return nil
 }
 
 func (s *Server) listen() {
+	for {
+		conn, err := s.Listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+
+			log.Fatal(err)
+		}
+		go s.handle(conn)
+	}
 }
 
 func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	rsp := []byte("HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello World!\n")
+
+	_, err := conn.Write(rsp)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+		return
+	}
 }
